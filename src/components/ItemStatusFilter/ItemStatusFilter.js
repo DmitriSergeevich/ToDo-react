90a3e0518c9002115c19ebdfe7189c6592724248
Dmitry Likhachev <valomon@yandex.ru>
2021-08-09 12:44:08 +0500

    upgraded function-components itemStatusFilter and To do list Item  to class-components

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button" className="btn btn-info">All</button>
-      <button type="button" className="btn outline-secondary">Active</button>
-      <button type="button" className="btn outline-secondary">Done</button>
-    </div>
-  );
+export default class ItemStatusFilter extends Component {
+
+  render() {
+    return (
+      <div className="btn-group">
+        <button type="button" className="btn btn-info">All</button>
+        <button type="button" className="btn outline-secondary">Active</button>
+        <button type="button" className="btn outline-secondary">Done</button>
+      </div>
+    );
+  }
 }
--- src/components/ToDoListItem/ToDoListItem.js
+++ src/components/ToDoListItem/ToDoListItem.js
@@ -1,29 +1,36 @@
 import React from 'react';
 
 import './ToDoListItem.css'
-export const ToDoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'tomato' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
-  return (
-    <span className="todo-list-item">
-      <span 
-        className="todo-list-item-label"
-        style={style}>
-          { label }
-      </span>
 
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
+export default class ToDoListItem extends React.Component {
   
-    );
+
+  render() {
+    const {label, important = false} = this.props;
+    const style = {
+      color: important ? 'tomato' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+
+    return (
+      <span className="todo-list-item">
+        <span 
+          className="todo-list-item-label"
+          style={style}>
+            { label }
+        </span>
+  
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+  
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    
+      );
+  }
 }
